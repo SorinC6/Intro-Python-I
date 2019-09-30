b6606674d38248d96d720a298fc0543586f28dea
Sorin Chis <kish.sorin@yahoo.com>
2019-09-30 16:05:49 +0300

    added answers in 13_file.io.py file

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+foo = open('foo.txt', "r")
+content = foo.read()
+print(content)
+foo.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
+
+bar = open('bar.txt', "w")
+bar.write("First pharagraf\n")
+bar.write("Second pharagraf\n")
+bar.write("Third pharagraf")
+bar.close()
 
 # YOUR CODE HERE
